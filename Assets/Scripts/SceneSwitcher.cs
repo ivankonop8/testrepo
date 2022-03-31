2df250e81648ee48523054f5a7570d62c6a2510e
Ivan Konop <ivan.konop8@gmail.com>
2022-03-31 14:29:14 +0300

    initial version

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public string sceneName;
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
